31067641a7eb3a13b4214da9bf7d73d01cf193f7
Code-with-Nani <usmanhanan02@gmail.com>
2024-03-17 09:18:54 +0500

    button added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="main-nav bg-gray-600 h-[10vh] flex justify-between items-center px-[1.5vw]">
+    <div className="main-nav bg-gray-600 h-[10vh] flex justify-between items-center px-[1.5vw] select-none">
       <div className="left flex justify-center items-center gap-1">
         <img className="h-[1vw]" src="/smile.svg" alt="Icon" />
         <h2 className="Helvetica-robot uppercase text-[0.9vw] text-white">
@@ -13,17 +13,12 @@ const Header = () => {
         <img className="h-[1.5vw] invert" src="/logo.svg" alt="Icon" />
       </div>
       <div className="right">
-        <li cl>
-
-        </li>
-        
+        <button className="Helvetica-robot uppercase text-[0.9vw] text-[#75F348] border-[1.5px] border-[#75F348] px-[8px] py-[2px] rounded-[5px] hover:text-black relative after:absolute after:content-[''] after:h-[100%] after:w-[100%] after:bg-[#75F348] after:left-0 after:bottom-[-100%]  hover:after:bottom-0 hover:after:rounded-none after:transition-all after:ease after:duration-[0.5s] delay-100 overflow-hidden after:rounded-[5px] ">
+          <span className="z-10 relative">Menu</span>
+        </button>
       </div>
     </div>
   );
 };
 
 export default Header;
-
-
-
-// <button className="Helvetica-robot uppercase text-[0.9vw] text-[#75F348] border-[1.5px] border-[#75F348] px-[8px] py-[2px] rounded-[5px] hover:bg-[#75F348] hover:text-black ">Menu</button>
